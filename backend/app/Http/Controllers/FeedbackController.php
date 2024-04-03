209590de769d536f96b7786e6edd0aa3d28c0e1d
Ahmad Saleem <asaleem029@gmail.com>
2024-04-04 02:44:55 +0500

    Feedback CRUD implemented

--- backend/app/Http/Controllers/FeedbackController.php
+++ backend/app/Http/Controllers/FeedbackController.php
@@ -2,33 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Feedback;
+use App\Http\Services\FeedbackService;
 use Illuminate\Http\Request;
 
 class FeedbackController extends Controller
 {
+    protected $feedbackService;
+
+    public function __construct(FeedbackService $feedbackService)
+    {
+        $this->feedbackService = $feedbackService;
+    }
+
     public function index()
     {
-        //
+        return $this->feedbackService->feedbacksList();
     }
-
+    
     public function store(Request $request)
     {
-        //
+        return $this->feedbackService->createfeedback($request);
     }
 
-    public function show(Feedback $feedback)
+    public function show($id)
     {
-        //
+        return $this->feedbackService->getfeedback($id);
     }
 
-    public function update(Request $request, Feedback $feedback)
+    public function update(Request $request)
     {
-        //
+        return $this->feedbackService->updatefeedback($request);
     }
 
-    public function destroy(Feedback $feedback)
-    {
-        //
+    public function destroy($id) {
+        return $this->feedbackService->deletefeedback($id);
     }
 }
---
+++ backend/app/Http/Services/FeedbackService.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Services;
+
+use App\Models\Feedback;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class FeedbackService
+{
+    public function feedbacksList()
+    {
+        try {
+            $user_id = Auth::id();
+            $feedbacks = Feedback::where('user_id', $user_id)->get();
+
+            if ($feedbacks)
+                return response()->json([
+                    'feedbacks' => $feedbacks,
+                ], 200);
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function createFeedback($data)
+    {
+        try {
+            // Validate request
+            $validator = Validator::make($data->all(), [
+                'title' => 'required|string|max:100',
+                'description' => 'required|string|max:100',
+                'category' => 'required|string|max:100',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json($validator->errors()->toJson(), 400);
+            }
+
+            $input = $data->all();
+            $input['user_id'] = Auth::id();
+
+            // Create new user
+            $feedback = Feedback::create($input);
+
+            if ($feedback)
+                return response()->json([
+                    'message' => 'New Feedback Added',
+                ], 200);
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function getFeedback($id)
+    {
+        try {
+            $feedback = Feedback::find($id);
+
+            if ($feedback)
+                return response()->json([
+                    'feedback' => $feedback,
+                ], 200);
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function updateFeedback($data)
+    {
+        try {
+            // Validate request
+            $validator = Validator::make($data->all(), [
+                'title' => 'required|string|max:100',
+                'description' => 'required|string|max:100',
+                'category' => 'required|string|max:100',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json($validator->errors()->toJson(), 400);
+            }
+
+            $feedback = Feedback::find($data->id);
+            $feedback->title = $data->title;
+            $feedback->description = $data->description;
+            $feedback->category = $data->category;
+            $feedback->user_id = Auth::id();
+            $feedback->save();
+
+            if ($feedback)
+                return response()->json([
+                    'message' => 'Feedback Info Updated',
+                    'feedback' => $feedback,
+                ], 200);
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function deleteFeedback($id)
+    {
+        try {
+            $feedback = Feedback::find($id);
+            $feedback->delete();
+
+            return response()->json([
+                'message' => 'Feedback deleted successfully',
+            ], 200);
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+}
